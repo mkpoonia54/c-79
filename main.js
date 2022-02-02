c4a3ec302feaf9b311cd0a15538c87aa027913a9
mkpoonia54 <98869204+mkpoonia54@users.noreply.github.com>
2022-02-02 11:40:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+a=[];
+function sub(){ 
+
+var n1=document.getElementById("student1").value;
+var n2=document.getElementById("student2").value;
+a.push (n1);
+a.push (n2);
+console.log(a);
+}
+
+function sorting(){
+   a.sort();
+   console.log(a);
+
+
+
+
+
+
+}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                        
